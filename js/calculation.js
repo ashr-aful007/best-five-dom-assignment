0224d4a28513017d0a1012a5ae7a0b75c2b050f8
ashr-aful007 <ashraful99980@gmail.com>
2022-08-23 22:27:45 +0600

    fix disibal button

--- js/calculation.js
+++ js/calculation.js
@@ -5,7 +5,7 @@ function addPlayerName(names){
    
      const playerList = document.getElementById('player-list');
      playerList.innerHTML = '';
-        //validation andt alart for 5 player selection
+        //validation and alart for 5 player selection
         if(playerNames.length == 6){
           alert('you alrady select 5 player')
      }
@@ -22,17 +22,20 @@ function addPlayerName(names){
      }
 }
 
-
-
-//de
-// const buttons = document.querySelectorAll('a')
-// for(let button of buttons){
-//      button.setAttribute('disabled');
-//      console.log(button)
+// function selectButton(){
+//      clickedBtnlist =  document.querySelector('a')
+//      let clickedlist;
+//      for(let i = 0; i<clickedBtnlist.langth; i++){
+//         clickedlist = clickedBtnlist[i]
+//         console.log(clickedlist)
+//      }
+//      return clickedlist;
+     
 // }
 
 
 
+//evetn function
 function btnClick(element){    
  
      const playerName = element.parentNode.children[0].innerText;
@@ -44,10 +47,13 @@ function btnClick(element){
      document.getElementById('selection-number').innerText = playerNames.length;
 
      addPlayerName(playerNames)
-
-   const clickedBtn =  document.querySelector('a');
-   clickedBtn.style.pointerEvents = 'none';
-   clickedBtn.style.backgroundColor = 'lightgray';
+     const clickedBtn =  document.querySelector('a');
+     for (let i = 0; i <clickedBtn.length; i++){
+          const element = clickedBtn[i];
+          
+     }    
+     element.style.pointerEvents = 'none';
+     element.style.backgroundColor = 'lightgray';
    
 
    
@@ -64,6 +70,7 @@ function getInputFildValueWithId(inputFlidId){
      
 }
 
+//validation function
 function forValidation(inputValue){
    if(isNaN(inputValue)){
      alert('plese give valid number')
@@ -76,6 +83,7 @@ function forValidation(inputValue){
    }
 }
 
+//calculation function
 function playerExpenses(){
      const inputFldParPlayer =  getInputFildValueWithId('Per-player');
      const perPlayerCost = playerNames.length * inputFldParPlayer;
@@ -94,6 +102,7 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
      
 })
 
+//calculate fanction
 document.getElementById('calculate-Total').addEventListener('click', function(){
 
      const parplayerExpns = playerExpenses()
